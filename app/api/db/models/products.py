2b0b4ace38b3a9059f4deabcbbf965410e099cfe
Татьяна Сарафанова <sara4ka0@yandex.ru>
2024-03-23 20:26:55 +0300

    add description in db model

--- app/api/db/models/products.py
+++ app/api/db/models/products.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, Text
 
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -9,3 +9,8 @@ class Product(Base):
     title: Mapped[str] = mapped_column(String(30))
     price: Mapped[float]
     count: Mapped[int]
+    description: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
