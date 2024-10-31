6b3f1fba1ad4f8f4efd6fbfff9c4f2a409416e54
Sarafanova Tanya <tanya.sarafanova@gmail.com>
2024-10-31 15:19:22 +0300

    Add settings for testing auth

--- tests/test_db_settings.py
+++ tests/test_db_settings.py
@@ -9,13 +9,22 @@ class PostgresTestSettings(BaseSettings):
     port: int
 
     model_config = SettingsConfigDict(
-        env_file=".env.test", env_prefix="POSTGRES_", env_file_encoding="utf-8"
+        env_file=".env.test", env_prefix="POSTGRES_", env_file_encoding="utf-8",  extra='ignore'
     )
 
     def get_sqlalchemy_dsn(self) -> str:
         return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db}"
 
 
+class AuthTestSettings(BaseSettings):
+    key: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_prefix="SECRET_", env_file_encoding="utf-8", extra='ignore'
+    )
+
+
 test_postgres_settings = PostgresTestSettings()
+test_auth_settings = AuthTestSettings()
 
 TEST_DATABASE_URL = test_postgres_settings.get_sqlalchemy_dsn()
